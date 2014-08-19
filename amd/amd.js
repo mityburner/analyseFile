767c747fb5f4b3e7a90c0ac69b90909cf70ddd6a
mityburner <mityburner@gmail.com>
2014-08-19 13:40:18 +0800

    amd.js实现AMD规范的最小代码
    对amd.js文件的分析，源码地址为：
    https://curiosity-driven.org/minimal-loader?utm_source=echojs

---
+++ amd/amd.js
@@ -0,0 +1,89 @@
+/*
+    https://curiosity-driven.org/minimal-loader?utm_source=echojs
+*/
+(function(document) {
+
+    /*listeners，模块请求容器，形式为：listeners[name] = [listener]，name为require(name, listener)的模块名称，listener为模块加载后要做的处理,数组表明模块会被请求多次
+    *resolves，解析信息容器，形式为：resolves[name] = value，name为模块名，value为解析后的值（或者说是js代码执行后的结果）
+    *config，配置信息容器，形式为：config[name] = url;name为模块名，url为模块对应的加载地址
+    */
+    var listeners = {}, resolves = {}, config = {};
+
+    /*加载模块*/
+    function addLoadListener(name, listener) {
+        //name模块已被解析，其值为(模块已加载，且返回值为)resolves[name]，则立即执行require(name, fn)中的fn
+        if(name in resolves) listener(name, resolves[name]);
+        //模块未被解析且曾被请求过，则将fn存储在listeners模块请求容器中
+        else if(listeners[name]) listeners[name].push(listener);
+        else {
+            //模块未被解析且未曾请求过，则将fn存储并该文件引入到网页中
+            listeners[name] = [listener];
+            if(config[name]) {
+                var element = document.createElement('SCRIPT');
+                element.src = config[name];
+                document.head.appendChild(element);
+                delete config[name];
+            }
+        }
+    }
+    /*解析(解释)模块*/
+    function resolve(name, value) {
+        //将解析后的值存储到resolves对象中，即，在name模块加载之后，将返回值value存储在resolves对象中
+        resolves[name] = value;
+        //判断模块是否被require(name, fn)请求过
+        if(listeners[name]) {
+            //将模块名name和返回值value作为参数，立即执行fn
+            listeners[name].forEach(function(listener) {
+                listener(name, value);
+            });
+            //从请求容器listeners中将name模块删除
+            delete listeners[name];
+        }
+    }
+
+    /*定义require()方法*/
+    function require(deps, definition) {
+        //没有依赖则直接执行definition()处理函数
+        if(!deps.length) definition();
+        else {
+            /*values存储解析后的值
+            loaded存储依赖模块累计加载数
+            */
+            var values = [], loaded = 0;
+            deps.forEach(function(dep) {
+                //分别加载模块
+                addLoadListener(dep, function(name, value) {
+                    //将每次加载模块后返回的值value进行存储
+                    values[deps.indexOf(name)] = value;
+                    //所有依赖模块加载完毕，执行自定义的处理函数definition()
+                    if(++loaded >= deps.length) definition.apply(null, values);
+                });
+            });
+        }
+    }
+
+    /*定义define()方法*/
+    function define(name, deps, definition) {
+        //只有两个参数的时候，即，没有依赖的时候做出的处理
+        if(!definition) {
+            definition = deps;
+            deps = [];
+        }
+        //没有依赖则直接解析
+        if(!deps.length) resolve(name, definition());
+        else {
+            //有依赖时，则请求依赖的模块
+            require(deps, function() {
+                resolve(name, definition.apply(null, arguments));
+            });
+        }
+    }
+    /*定义require.config()方法
+    将配置文件存储在config对象中*/
+    require.config = function(map) {
+        for(var n in map) config[n] = map[n];
+    };
+
+    window.require = require;
+    window.define = define;
+}(document));
